1f15f35400dd5116606035f07f42523d2bc6b719
sam1502 <sameer.neema02@gmail.com>
2018-03-25 15:40:26 -0500

    initial commit

---
+++ src/main/java/com/emailingservice/EmailAPI/EmailApiApplication.java
@@ -0,0 +1,12 @@
+package com.emailingservice.EmailAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailApiApplication.class, args);
+	}
+}
